0fe555bf95e5e9324579bfd8f3d62589c132ca82
Sreejith Sreedaharan <sreejith.m.sreedharan@gmail.com>
2020-07-23 20:38:28 +0530

    synaptic integration

--- src/routes/random.js
+++ src/routes/random.js
@@ -1,8 +1,23 @@
+const engine = require('../services/engine');
 
-module.exports =  function (app, router){
-    
-    router.use('/predict', function(req, res , next){
-        return res.json({"status":"ok"});
+module.exports = function (app, router) {
+
+    router.use('/predict', function (req, res, next) {
+        const mode = req.query.mode;
+        const userId = req.query.userId || 0;
+        const response = engine().predict(mode, userId);
+        if (response && response.prediction && response.prediction.data) {
+            response.prediction.data = Buffer.from(response.prediction.data).toString('base64');
+        }
+        return res.json(response);
+    });
+    router.use('/feedback', function (req, res, next) {
+        const mode = req.query.mode;
+        let feedback = req.query.feedback;
+        const userId = req.query.userId || 0;
+        feedback = Buffer.from(decodeURIComponent(feedback || "") || "", 'base64').toString('ascii');
+        const response = engine().feedback(mode, feedback, userId);
+        return res.json(response);
     });
     return router;
 }
